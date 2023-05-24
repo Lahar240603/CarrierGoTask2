f8e64a306c67d8de622404f97f4c691ad13bebbf
iakshitsharma210 <iakshitsharma210@gmail.com>
2023-05-24 12:06:55 +0530

    Controller update for both Amend and Update

---
+++ carriergot2/src/main/java/com/task2/carriergot2/service/impl/WorkOrderStateDeciderGetOrg.java
@@ -0,0 +1,36 @@
+package com.task2.carriergot2.service.impl;
+
+
+import com.task2.carriergot2.model.AmendWorkOrderStateDecider;
+import com.task2.carriergot2.model.UpdateWorkOrderStateDecider;
+import com.task2.carriergot2.service.iAmendServiceGetOrg;
+import com.task2.carriergot2.service.iUpdateServiceGetOrg;
+import com.task2.carriergot2.utils.WorkOrderStateDeciderRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.task2.carriergot2.service.iWorkOrderStateDeciderGetOrg;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+public class WorkOrderStateDeciderGetOrg implements iWorkOrderStateDeciderGetOrg {
+
+    @Autowired
+    private iAmendServiceGetOrg amendedService ;
+
+    @Autowired
+    private iUpdateServiceGetOrg updatedService;
+
+    @Override
+//    @Transactional
+    public WorkOrderStateDeciderRequest getOrgRequest(String getOrg){
+        List<UpdateWorkOrderStateDecider> updatedByOrgCode = updatedService.findByOrgCode(getOrg);
+        List<AmendWorkOrderStateDecider> amendedByOrgCode = amendedService.findByOrgCode(getOrg);
+
+        return new WorkOrderStateDeciderRequest(amendedByOrgCode , updatedByOrgCode);
+    }
+
+
+
+}
