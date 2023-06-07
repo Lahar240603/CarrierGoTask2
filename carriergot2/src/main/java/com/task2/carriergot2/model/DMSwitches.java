23418c00fdb89ee3fc8231b5091b8421a1eb3037
NihithaReddyC <135799629+NihithaReddyC@users.noreply.github.com>
2023-06-07 11:39:11 +0530

    Add files via upload

---
+++ carriergot2/src/main/java/com/task2/carriergot2/model/DMSwitches.java
@@ -0,0 +1,101 @@
+/*package com.example.demo3.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="dmswitch")
+public class DMSwitches {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="SWITCHNAME")
+     private String switchName;
+	
+	@Column(name="ISENABLED")
+    private boolean isEnabled;
+
+	public DMSwitches() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public DMSwitches(String switchName, boolean isEnabled) {
+		super();
+		this.switchName = switchName;
+		this.isEnabled = isEnabled;
+	}
+
+	public String getSwitchName() {
+		return switchName;
+	}
+
+	public void setSwitchName(String switchName) {
+		this.switchName = switchName;
+	}
+
+	public boolean isEnabled() {
+		return isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	@Override
+	public String toString() {
+		return "DMSwitches [switchName=" + switchName + ", isEnabled=" + isEnabled + "]";
+	}
+}*/
+package com.example.demo3.entity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+@Entity
+@Table(name="dmswitch")
+public class DMSwitches {
+	
+	@Id
+	@Column(name="SWITCHNAME")
+	private String switchName;
+	
+	@Column(name="ISENABLED")
+	private boolean isEnabled;
+
+	public DMSwitches() {
+		super();
+	}
+
+	public DMSwitches(String switchName, boolean isEnabled) {
+		super();
+		this.switchName = switchName;
+		this.isEnabled = isEnabled;
+	}
+
+	public String getSwitchName() {
+		return switchName;
+	}
+
+	public void setSwitchName(String switchName) {
+		this.switchName = switchName;
+	}
+
+	public boolean getIsEnabled() {
+		return isEnabled;
+	}
+
+	public void setIsEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	@Override
+	public String toString() {
+		return "DMSwitches [switchName=" + switchName + ", isEnabled=" + isEnabled + "]";
+	}
+}
+
