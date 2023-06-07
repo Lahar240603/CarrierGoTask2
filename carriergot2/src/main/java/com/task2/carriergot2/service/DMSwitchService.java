dbd42fa74ad2751b517053b8210ae98aa817aa61
NihithaReddyC <135799629+NihithaReddyC@users.noreply.github.com>
2023-06-07 11:40:43 +0530

    Add files via upload

---
+++ carriergot2/src/main/java/com/task2/carriergot2/service/DMSwitchService.java
@@ -0,0 +1,87 @@
+package com.example.demo3.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo3.entity.DMSwitches;
+import com.example.demo3.repository.DMSwitchRepository;
+
+@Service
+public class DMSwitchService {
+	
+	@Autowired
+	private final DMSwitchRepository dmswitchRepository;
+
+    @Autowired
+    public DMSwitchService(DMSwitchRepository dmswitchRepository) {
+        this.dmswitchRepository = dmswitchRepository;
+    }
+
+    
+    public List<DMSwitches> getALLDMSwitches() {
+        return (List<DMSwitches>) dmswitchRepository.findAll();
+    }
+
+    
+    public DMSwitches getDMSwitchByName(String switchName) {
+        return dmswitchRepository.findBySwitchName(switchName);
+    }
+
+   
+    
+    /*public DMSwitches addOrUpdateDMSwitch(DMSwitches dmSwitch) {
+        return dmswitchRepository.save(dmSwitch);
+    }*/
+
+    public DMSwitches addSwitch(DMSwitches dmswitch) {
+      DMSwitches newSwitch = new DMSwitches();
+              newSwitch.setSwitchName(dmswitch.getSwitchName());
+              newSwitch.setIsEnabled(dmswitch.getIsEnabled());       	
+         return dmswitchRepository.save(newSwitch);
+         }
+//        catch (Exception ex) {
+//            ex.printStackTrace();
+//            return null;
+//        }
+   
+
+
+    public DMSwitches updateSwitch(DMSwitches dmswitch) {
+        Optional<DMSwitches> existingSwitch = dmswitchRepository.findById(dmswitch.getSwitchName());
+        if (existingSwitch.isPresent()) {
+            return dmswitchRepository.save(dmswitch);
+        }
+        return null; 
+    }
+
+    
+    public DMSwitches deleteDMSwitch(String switchName) throws Exception {
+        DMSwitches existingSwitch = dmswitchRepository.findBySwitchName(switchName);
+        if (existingSwitch != null) {
+            dmswitchRepository.delete(existingSwitch);
+        } else {
+            throw new Exception("Switch not found");
+        }
+        return existingSwitch;
+    }
+
+
+	
+
+
+	/*public DMSwitches addSwitch(DMSwitches dmswitch) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public DMSwitches updateSwitch(DMSwitches dmswitch) {
+		// TODO Auto-generated method stub
+		return null;
+	}*/
+	
+}
+
